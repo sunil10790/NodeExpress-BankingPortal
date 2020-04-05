9224380d16ce858a76eed8cdfab849aa00138769
Sunil <sunil10790@gmail.com>
2020-04-05 19:08:06 +0530

    Create an Index route

--- src/app.js
+++ src/app.js
@@ -8,3 +8,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index', {title: 'Index'});
+})
